da99903d1b72b9ba8f5df535c1be907412811488
Steve Wolligandt <stevewolligandt@gmail.com>
2020-04-02 15:10:08 +0200

    -

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,40 @@ alias cninja 'cmake -GNinja'
 alias ccninja 'ccmake -GNinja'
 
 source /usr/share/autojump/autojump.fish
+
+
+set -g theme_display_git yes
+#set -g theme_display_git_dirty yes
+#set -g theme_display_git_untracked yes
+#set -g theme_display_git_ahead_verbose yes
+#set -g theme_display_git_dirty_verbose yes
+#set -g theme_display_git_stashed_verbose yes
+#set -g theme_display_git_master_branch yes
+#set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+set -g theme_display_nvm yes
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_vi no
+set -g theme_display_date yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path yes
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path yes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g theme_color_scheme dark
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 1
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
