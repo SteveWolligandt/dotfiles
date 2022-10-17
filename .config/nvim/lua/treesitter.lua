4fe485223e25729ed5c520f180fc4d22dbf13acc
Steve Wolligandt <steve@isg.cs.ovgu.de>
2022-10-17 16:56:26 +0200

    -

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,8 +1,8 @@
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "norg", "haskell", "cpp", "c", "javascript", "markdown" },
-    highlight = { -- Be sure to enable highlights if you haven't!
-        enable = true,
-    }
+  ensure_installed = { "norg", "haskell", "cpp", "c", "javascript", "markdown" },
+  highlight = { -- Be sure to enable highlights if you haven't!
+    enable = true,
+  }
 }
