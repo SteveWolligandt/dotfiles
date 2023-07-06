03ee3c5c1aa02f646f311f7c28ce96a09c59b670
Steve Wolligandt <steve.wolligandt@siemens.com>
2023-07-06 12:10:18 +0200

    bash treesitter support

--- .config/nvim/lua/config/treesitter/init.lua
+++ .config/nvim/lua/config/treesitter/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'cpp', 'c', 'python', 'lua', 'java' },
+    ensure_installed = { 'cpp', 'c', 'python', 'lua', 'java', 'bash' },
     highlight = { enable = true, }
   }
 end
